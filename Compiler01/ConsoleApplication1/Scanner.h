bc326927245ecae644ced4ccb77632c0c215c1af
danners91 <hoangdan91@gmail.com>
2017-09-14 17:57:17 -0700

    Update Scanner.h

--- Compiler01/ConsoleApplication1/Scanner.h
+++ Compiler01/ConsoleApplication1/Scanner.h
@@ -8,11 +8,27 @@ using namespace std;
 
 //change functions into virtual for scanner
 
-class scanner : public source
+class scanner :public source
 {
+Public: 
+scanner(source *source); // default constructor
+
+virtual ~scanner(); // destructor
+
+token *currentToken(); // returns current token
+
+token *nexttoken(token *prevToken) throw (string); // returns previous token, and throws a string if error happens
+
+char currentChar() throw (string); // return current character, throw string if error
+
+char nextChar() throw (string); // return next character, throw string if error
+
+
+
 protected:
 	virtual token currentToken(void);
 	virtual token nextToken(void);
+	virtual token prevToken(void);
 	token extractToken(void);
 	char currentChar(void);
 	char nextChar(void);
