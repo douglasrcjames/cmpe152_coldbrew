4cc6d0645509860da7d30c074959df353f488f43
danners91 <hoangdan91@gmail.com>
2017-09-15 13:19:05 -0700

    Update Token.h

--- Compiler01/ConsoleApplication1/Token.h
+++ Compiler01/ConsoleApplication1/Token.h
@@ -5,10 +5,29 @@
 
 using namespace std;
 
+enum class tokentype
+{
+    // subclass of what token can be
+};
+
 class token : public source
 {
 public:
 	token();
+	
+	virtual ~token(); // destructor
+	
+	tokentype get_type() const; //return the token type
+	
+	string get_text() const; // return token text
+	
+	int get_line_num() const; //return the source line #
+	
+	int get_position() const; // return position
+	
+	
+	
+	
 	
 protected:
 	string text;
@@ -17,6 +36,10 @@ protected:
 	char currentChar(void); //done
 	char nextChar(void);	//done
 	char peekChar(void);	//done
+	
+	tokentype type; // language specific token
+	int line_number; // line # of tokens source line
+	int position; // position of 1st character token
 
 private:
 	int lineNum;
